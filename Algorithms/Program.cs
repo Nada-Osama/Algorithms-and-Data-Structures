2fc7ab6402b5df0a9b91c6a78eff3bffe011f1cc
Nada Osama <nosamaosman@gmail.com>
2019-07-08 00:05:10 +0200

    binary search 1

---
+++ Algorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the item you are searching for: ");
+            int numberExistence;
+            String word;
+            String[] wordsList = { "name", "judgment", "office", "box", "fire" };
+            int[] numbersList = { 1, 3, 5, 7, 9 };
+            Array.Sort(wordsList);
+            Array.Sort(numbersList);
+            word = Console.ReadLine();
+            Console.WriteLine(SearchingAlgorithms<String>.Binary_Search(wordsList, word));
+        }
+    }
+}
---
+++ Algorithms/SearchingAlgorithms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class SearchingAlgorithms<T>
+    {
+        public static String Binary_Search (T[] list, T item)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+            int mid, low = 0, high = list.Length - 1;
+            while ( low <= high )
+            {
+                mid = (low + high) / 2;
+                if (comparer.Compare(list[mid], item) == 0)
+                    return item + " found at index " + mid;
+                if (comparer.Compare(list[mid], item) > 0)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            return item + " not found in this list!";
+        }
+    }
+}

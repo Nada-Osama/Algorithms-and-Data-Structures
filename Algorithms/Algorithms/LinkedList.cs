2c61ee60e9e71258aa2b84763b4372d495bd3dc5
Nada Osama <nosamaosman@gmail.com>
2019-07-10 19:07:59 +0200

    LinkedList task added

---
+++ Algorithms/Algorithms/LinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Link<T1, T2>
+    {
+        /// <summary>
+        /// <para data1, data2> is the data by which list will be filled in </para>
+        /// </summary>
+        private T1 data1;
+        private T2 data2;
+        /// <summary>
+        /// <para next> referes to the next item in list </para>
+        /// </summary>
+        public Link<T1, T2> next;
+        public Link(T1 dt1, T2 dt2)
+        {
+            data1 = dt1;
+            data2 = dt2;
+        }
+        public void printLink()  //prints itself
+        {
+            Console.WriteLine("{" +data1 + ", " +data2 + "}");
+        }
+    }
+
+    class LinkedList<T1,T2>
+    {
+        private Link<T1,T2> first;  // referes to first link in list
+        public LinkedList ()
+        {
+            first = null;  //no items in list yet
+        }
+        public Boolean isEmpty ()
+        {
+            return (first == null);
+        }
+        public void insertFirst (T1 data1, T2 data2)  // insert new link at start of list
+        {
+            Link<T1, T2> newLink = new Link<T1, T2>(data1, data2);
+            newLink.next = first;    // newLink --> old first 
+            first = newLink;        // first --> newLink
+        }
+        public Link<T1, T2> deleteFirst ()  //delete first item in list
+        {
+            Link<T1, T2> deletedItem = first;
+            first = first.next;
+            return deletedItem;
+        }
+        public void displayList ()
+        {
+            Console.WriteLine("items in list: ");
+            Link<T1, T2> current = first;
+            while (current != null)
+            {
+                current.printLink();
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -10,33 +10,64 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            ///<summary>
-            /// Binary Search testing.
-            ///</summary>
-            Console.Write("Enter the item you are searching for: ");
-            int numberExistence, position;
-            String word;
-            int[] numbersList = { 1, 3, 5, 7, 9 };
-            String[] wordsList = { "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg" };
-            numberExistence = int.Parse(Console.ReadLine());
-            position = SearchingAlgorithms<int>.Binary_Search(numbersList, numberExistence);
-            if (position == -1)
-                Console.WriteLine("{0} not found in this list!", numberExistence);
-            else
-            {
-                Console.WriteLine("{0} is found at index {1}", numberExistence, position);
-            }
+            /////<summary>
+            ///// Binary Search testing.
+            /////</summary>
+            //Console.Write("Enter the item you are searching for: ");
+            //int numberExistence, position;
+            //String word;
+            //int[] numbersList = { 1, 3, 5, 7, 9 };
+            //String[] wordsList = { "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg" };
+            //numberExistence = int.Parse(Console.ReadLine());
+            //position = SearchingAlgorithms<int>.Binary_Search(numbersList, numberExistence);
+            //if (position == -1)
+            //    Console.WriteLine("{0} not found in this list!", numberExistence);
+            //else
+            //{
+            //    Console.WriteLine("{0} is found at index {1}", numberExistence, position);
+            //}
+
+
+            ////=============================================================================================
+
+            /////<summary>
+            ///// Stack testing.
+            /////</summary>
+            //string input;
+            //Console.Write("Enter the string you want to reverse: ");
+            //input = Console.ReadLine();
+            //StackExample stk = new StackExample(input);
+            //Console.WriteLine("Your string after reversing is: {0}", stk.Reverse());
 
             //=============================================================================================
 
+
             ///<summary>
-            /// Stack testing.
+            /// LinkedList testing.
             ///</summary>
-            string input;
-            Console.Write("Enter the string you want to reverse: ");
-            input = Console.ReadLine();
-            StackExample stk = new StackExample(input);
-            Console.WriteLine("Your string after reversing is: {0}", stk.Reverse());
+            LinkedList<int, double> fillList = new LinkedList<int, double>();
+            Link<int, double> deleteList;  //param used to store deleted links
+
+            fillList.insertFirst(22, 2.99);  //filling the list
+            fillList.insertFirst(44, 4.99);
+            fillList.insertFirst(66, 6.99);
+            fillList.insertFirst(88, 8.99);
+
+            fillList.displayList();  //display list items after filling it
+
+            while (!fillList.isEmpty())  //deleting list links
+            {
+                deleteList = fillList.deleteFirst();  //store deleted links to display them
+                Console.Write("Deleted ");
+                deleteList.printLink();  //print deleted link
+            }
+
+            Console.WriteLine();
+            fillList.displayList();  //display list items after deleting it (no items will be found)
+  
+
+            //============================================================================================
+
 
         }
     }
